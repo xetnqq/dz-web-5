850675f3f4721a1f7b4945e28cde50d55e0ed644
xetnqq <rostykfuc@gmail.com>
2023-12-12 22:33:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function convertToImages() {
+    var text = document.getElementById("source-text").value;
+    var resultText = document.getElementById("result-text");
+    resultText.innerHTML = ""; // Очищення результатів при кожному конвертуванні
+
+    for (var i = 0; i < text.length; i++) {
+        var image = document.createElement("img");
+        var imagePath = "images/" + text[i].toLowerCase() + ".png";
+        image.src = imagePath;
+        image.alt = text[i];
+
+        resultText.appendChild(image);
+    }
+}
